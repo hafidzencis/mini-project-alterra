a5a1ae63abef2f1a93ba25760ea7abf0fa618952
hafidzencis <hafidzfebrian21@gmail.com>
2022-04-22 13:38:40 +0700

    cicd jacoco rev 1

--- src/main/java/com/alterra/cicdjacoco/controller/BasicController.java
+++ src/main/java/com/alterra/cicdjacoco/controller/BasicController.java
@@ -3,6 +3,7 @@ package com.alterra.cicdjacoco.controller;
 import com.alterra.cicdjacoco.service.BasicService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,6 +14,8 @@ public class BasicController {
     @Autowired
     private BasicService basicService;
 
+
+    @GetMapping(value = "")
     public ResponseEntity<Object> basic(){
         return basicService.basic();
     }
