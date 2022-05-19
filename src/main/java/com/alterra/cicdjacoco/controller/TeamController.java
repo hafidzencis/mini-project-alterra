febce65ca17094adec7090bbe76b9228bc04c442
hafidzencis <hafidzfebrian21@gmail.com>
2022-05-19 14:47:20 +0700

    add service, controller

---
+++ src/main/java/com/alterra/cicdjacoco/controller/TeamController.java
@@ -0,0 +1,90 @@
+package com.alterra.cicdjacoco.controller;
+
+import com.alterra.cicdjacoco.constantapp.ResponseMassage;
+import com.alterra.cicdjacoco.domain.dao.TeamDao;
+import com.alterra.cicdjacoco.domain.dao.UserDao;
+import com.alterra.cicdjacoco.domain.dto.TeamDto;
+import com.alterra.cicdjacoco.repository.TeamRepository;
+import com.alterra.cicdjacoco.service.TeamService;
+import com.alterra.cicdjacoco.service.UserService;
+import com.alterra.cicdjacoco.util.ResponseUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.security.Principal;
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/v1/team")
+
+public class TeamController {
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private TeamRepository teamRepository;
+
+    @Autowired
+    private TeamService teamService;
+
+    @PostMapping(value = "")
+    public ResponseEntity<Object> createNewTeam(Principal principal,@RequestBody TeamDto request){
+
+        UserDao user = (UserDao) userService.loadUserByUsername(principal.getName());
+        if (user.getAuthor().equals("ADMIN")){
+            return teamService.createNewTeam(request);
+        }
+        return ResponseUtil.build(ResponseMassage.NON_AUTHORIZED,null, HttpStatus.BAD_REQUEST);
+
+    }
+
+    @GetMapping(value = "")
+    public ResponseEntity<Object> getAllTeam(Principal principal){
+
+        UserDao user = (UserDao) userService.loadUserByUsername(principal.getName());
+        if (user.getAuthor().equals("ADMIN")){
+            return teamService.getAllTeam();
+        }
+        return ResponseUtil.build(ResponseMassage.NON_AUTHORIZED,null, HttpStatus.BAD_REQUEST);
+
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Object> getTeamById(Principal principal,@PathVariable Long id){
+
+        UserDao user = (UserDao) userService.loadUserByUsername(principal.getName());
+        if (user.getAuthor().equals("ADMIN")){
+            return teamService.getTeambyId(id);
+        }
+        return ResponseUtil.build(ResponseMassage.NON_AUTHORIZED,null, HttpStatus.BAD_REQUEST);
+
+    }
+
+    @GetMapping(value = "/search")
+    public ResponseEntity<Object> findTeamByName(@RequestParam(value = "team_name",required = false) String teamName){
+        return teamService.findTeamByName(teamName);
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<Object> updateTeam(Principal principal,@PathVariable Long id,@RequestBody TeamDto request){
+        UserDao user = (UserDao) userService.loadUserByUsername(principal.getName());
+        if (user.getAuthor().equals("ADMIN")){
+            return teamService.updateTeam(id,request);
+        }
+        return ResponseUtil.build(ResponseMassage.NON_AUTHORIZED,null, HttpStatus.BAD_REQUEST);
+
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Object> deleteTeam(Principal principal, @PathVariable Long id){
+
+        UserDao user = (UserDao) userService.loadUserByUsername(principal.getName());
+        if (user.getAuthor().equals("ADMIN")){
+            return teamService.deleteTeam(id);
+        }
+        return ResponseUtil.build(ResponseMassage.NON_AUTHORIZED,null, HttpStatus.BAD_REQUEST);
+
+    }
+}
---
+++ src/main/java/com/alterra/cicdjacoco/service/ScheduleService.java
@@ -0,0 +1,130 @@
+package com.alterra.cicdjacoco.service;
+
+
+import com.alterra.cicdjacoco.constantapp.ResponseMassage;
+import com.alterra.cicdjacoco.domain.dao.ChildDao;
+import com.alterra.cicdjacoco.domain.dao.ScheduleDao;
+import com.alterra.cicdjacoco.domain.dto.ChildDto;
+import com.alterra.cicdjacoco.domain.dto.ScheduleDto;
+import com.alterra.cicdjacoco.repository.ScheduleRepository;
+import com.alterra.cicdjacoco.util.ResponseUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Slf4j
+@Service
+public class ScheduleService {
+    @Autowired
+    private ScheduleRepository  scheduleRepository;
+
+    @Autowired
+    private ModelMapper mapper;
+
+    public ResponseEntity<Object> getScheduleById(Long id){
+        try{
+            log.info("Executing get schedule by id,Id : {}",id);
+            Optional<ScheduleDao> scheduleDaoOptional = scheduleRepository.findById(id);
+
+            if (scheduleDaoOptional.isEmpty()){
+                log.info("schedule not found");
+                return ResponseUtil.build(ResponseMassage.KEY_NOT_FOUND,null,HttpStatus.BAD_REQUEST);
+            }
+            ScheduleDao scheduleDao = scheduleDaoOptional.get();
+            return ResponseUtil.build(ResponseMassage.KEY_FOUND,mapper.map(scheduleDao,ScheduleDto.class),HttpStatus.OK);
+        }catch (Exception e){
+            log.error("Get an error by executing get schedule by id, Error : {}",e.getMessage());
+            return ResponseUtil.build(ResponseMassage.KEY_NOT_FOUND,null,HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    public ResponseEntity<Object> createNewSchedule(ScheduleDto request){
+        try{
+            log.info("Executing create New Schedule");
+            ScheduleDao scheduleDao = ScheduleDao.builder()
+                    .daySchedule(request.getDaySchedule())
+                    .timeSchedule(request.getTimeSchedule())
+                    .build();
+            scheduleRepository.save(scheduleDao);
+            return ResponseUtil.build(ResponseMassage.KEY_FOUND,mapper.map(scheduleDao,ScheduleDto.class), HttpStatus.OK);
+        }catch (Exception e){
+            log.error("Get An error by executing create New Schedule, Error : {}",e.getMessage());
+            return ResponseUtil.build(ResponseMassage.KEY_NOT_FOUND,null,HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+    }
+
+    public ResponseEntity<Object> getAllSchedule(){
+        try {
+            log.info("Executing by get all schedule");
+            List<ScheduleDao> scheduleDaoList = scheduleRepository.findAll();
+            List<ScheduleDto> scheduleDtoList = new ArrayList<>();
+
+            for(ScheduleDao schedule : scheduleDaoList){
+                scheduleDtoList.add(mapper.map(schedule,ScheduleDto.class));
+            }
+
+            return ResponseUtil.build(ResponseMassage.KEY_FOUND,scheduleDtoList,HttpStatus.OK);
+        }catch (Exception e){
+            log.error("Get an error by get all schedule, Error : {}",e.getMessage());
+            return ResponseUtil.build(ResponseMassage.KEY_NOT_FOUND,null,HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    public ResponseEntity<Object> updateSchedule(ScheduleDto request, Long id) {
+
+        try {
+            Optional<ScheduleDao> scheduleDaoOptional = scheduleRepository.findById(id);
+
+            if (scheduleDaoOptional.isEmpty()) {
+                log.info("schedule not found");
+                return ResponseUtil.build(ResponseMassage.KEY_NOT_FOUND, null, HttpStatus.BAD_REQUEST);
+            }
+
+            ScheduleDao scheduleDao = scheduleDaoOptional.get();
+            scheduleDao.setDaySchedule(request.getDaySchedule());
+            scheduleDao.setTimeSchedule(request.getTimeSchedule());
+            scheduleRepository.save(scheduleDao);
+            return ResponseUtil.build(ResponseMassage.KEY_FOUND, mapper.map(scheduleDao,ScheduleDto.class), HttpStatus.OK);
+
+
+        } catch (Exception e) {
+            log.error("Get an error by update child, Error: {}", e.getMessage());
+            return ResponseUtil.build(ResponseMassage.KEY_NOT_FOUND, null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+
+    public ResponseEntity<Object> deleteSchedule(Long id){
+        try {
+            log.info("Executing delete by id, Id : {}",id);
+            Optional<ScheduleDao> scheduleDaoOptional = scheduleRepository.findById(id);
+
+            if (scheduleDaoOptional.isEmpty()){
+                log.info("schedule not found");
+                return ResponseUtil.build(ResponseMassage.KEY_NOT_FOUND,null,HttpStatus.BAD_REQUEST);
+            }
+
+            scheduleRepository.delete(scheduleDaoOptional.get());
+            return ResponseUtil.build(ResponseMassage.KEY_FOUND,null,HttpStatus.OK);
+        }catch (Exception e){
+            log.error("Get An error delete schedule by id, Error : {}", e.getMessage());
+            return ResponseUtil.build(ResponseMassage.KEY_NOT_FOUND,null,HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+
+
+
+
+
+
+
+}
