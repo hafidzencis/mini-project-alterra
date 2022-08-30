05fdf982ce9db46ab24c0452c688475ac88c01e0
hafidzencis <hafidzfebrian21@gmail.com>
2022-08-30 21:31:59 +0700

    gas

--- src/main/java/com/alterra/cicdjacoco/CicdjacocoApplication.java
+++ src/main/java/com/alterra/cicdjacoco/CicdjacocoApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CicdjacocoApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(CicdjacocoApplication.class, args);
 	}
 
